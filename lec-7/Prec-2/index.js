f5eced86faccb9d4ddf0866508acb87ec660abc1
Chiragshiyani4490 <dhavalleelawala@gmail.com>
2024-03-18 15:44:52 +0530

    update

--- lec-7/Prec-2/index.js
+++ lec-7/Prec-2/index.js
@@ -2,11 +2,60 @@ const express = require('express');
 const port = 8081;
 const app = express();
 
+app.use(express.urlencoded());
+
+const studentsData = [
+  {
+    userid : 1,
+    name : 'Dhaval',
+    email : 'dhaval@gmail.com',
+    password : 'dhaval@123',
+    phone : 9157882121
+  },
+  {
+    userid : 2,
+    name : 'Yash',
+    email : 'yash@gmail.com',
+    password : 'yash@123',
+    phone : 7405623561
+  },
+  {
+    userid : 3,
+    name : 'Vrutika',
+    email : 'Vrutika@gmail.com',
+    password : 'Vrutika@123',
+    phone : 8750678965
+  }
+]
+
+
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-  res.render('index');
-})
+  res.render('index',{
+    student : studentsData
+  });
+});
+
+app.post('/insertdata',(req,res)=>{
+  console.log(req.body);
+  let userid = req.body.userid;
+  let name =  req.body.name;
+  let email = req.body.email;
+  let password = req.body.password;
+  let phone = req.body.phone;
+
+  const obj = {
+     userid : userid,
+     name : name,
+     email : email,
+     password : password,
+     phone : phone
+  }
+  studentsData.push(obj);
+  console.log("Student successfully add.");
+  res.redirect('back');
+});
 
 app.listen(port,(err)=>{
   if(err){
--- lec-7/prec-1/index.js
+++ lec-7/prec-1/index.js
@@ -3,16 +3,64 @@ const express = require('express')
 const port = 8040;
 
 const app = express();
+app.use(express.urlencoded());
+const studentData = [
+  {
+    rollno :1,
+    name : "Kunal",
+    coures : "Front End",
+    grade : "A+"
+  },
+  {
+    rollno :2,
+    name : "Dhaval",
+    coures : "Full Stack",
+    grade : "A++"
+  },
+  {
+    rollno :3,
+    name : "Radika",
+    coures : "Back End",
+    grade : "A"
+  }
+];
+
+
 
 app.set('view engine','ejs');
 
 app.get('/',(req,res)=>{
-  return res.render('index');
+  res.render('index',{
+    student : studentData
+  });
+});
+
+app.post('/insertdata',(req,res)=>{
+  console.log(req.body);
+
+  let rollno = req.body.rollno;
+  let name = req.body.name;
+  let coures = req.body.coures;
+  let grade = req.body.grade;
+
+  const obj = {
+    rollno : rollno,
+    name : name,
+    coures : coures,
+    grade : grade
+  }
+
+  studentData.push(obj);
+  console.log("Student Data add successfully.");
+
+  res.redirect('back');
+
 });
 
 app.listen(port,(err)=>{
   if(err){
     console.log("server not start in port.");
+    return false;
   }
   else
   {
